5cd1ddb4572245df59eed869b274de6ca84b878a
AmanJinjianan <18703698654@163.com>
2019-09-16 22:54:11 +0800

    控制页面初始2

--- app/src/main/java/com/qixiang/bleskip_teacher/ControllerActivity.java
+++ app/src/main/java/com/qixiang/bleskip_teacher/ControllerActivity.java
@@ -2,12 +2,22 @@ package com.qixiang.bleskip_teacher;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class ControllerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);// 隐藏标题
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+
+         WindowManager.LayoutParams.FLAG_FULLSCREEN);// 设置全屏
+
         setContentView(R.layout.layout_control);
+
     }
 }
